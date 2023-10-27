48cfa7e17895f48cf68ba49e09511baeedbc2ae8
YELANDAOKONG <yelandaokong@yldk.xyz>
2023-10-27 20:18:00 +0800

    Add Built in Regex

---
+++ RWLibrary/BuiltInRegex.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RWLibrary;
+
+public class BuiltInRegex
+{
+    public static readonly Regex String = new Regex("^\".*\"$");
+    public static readonly Regex Char = new Regex("^\'.*\'$");
+}
